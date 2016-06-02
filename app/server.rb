a821ce8814e904ce52c6b439ec67406ad43d5b1d
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-06-02 19:52:43 +0900

    implement echo server

--- app/server.rb
+++ app/server.rb
@@ -1,19 +1,34 @@
 require 'em-websocket'
 require 'thin'
 require 'sinatra'
+require 'json'
 
 set :public_folder, './app'
 
-
 EM.run do
-  class App < Sinatra::Application
-    get "/" do
-      File.read('app/index.html')
+    class App < Sinatra::Application
+        get "/" do
+            File.read('app/index.html')
+        end
     end
-  end
-  server = Thin::Server.start App, '0.0.0.0', 9000
+    server = Thin::Server.start App, '0.0.0.0', 9000
 
+    connections = []
 
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 3000) do |ws|
-  end
-end  
+    EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 3000) do |ws|
+        ws.onopen do
+            puts "Connection opened"
+            connections << ws
+        end
+
+        ws.onclose { puts "Connection closed" }
+
+        ws.onmessage do |msg|
+            connections.each do |conn|
+                send_msg = {"data" => msg}
+                conn.send(send_msg.to_json)
+            end
+            puts "Recieved message: #{msg}"
+        end
+    end
+end
